f2bbf46e578f3fcbffe4bd7797e00bb70dabcf75
sahil07-rg <sahilkumarjnv12@gmail.com>
2026-03-24 00:42:55 +0530

    Update segmentation.py

--- src/segmentation.py
+++ src/segmentation.py
@@ -1,22 +1,17 @@
-#Creating segments now:
-Fs = 20480
-sig2 = cleaned_data[:, 2]
+ segments = []
+    for start in range(0, len(signals) - window_size + 1, window_size):
+        segment = signals[start:start + window_size, :]
+        segments.append(segment)
 
-window_size = Fs // 200
-segments = []
+    # -------- FEATURE EXTRACTION --------
+    X = []
+    for segment in segments:
+        X.append(extract_features(segment, Fs))
 
-for start in range(0, len(sig2) - window_size + 1, window_size):
-    segment = sig2[start:start + window_size]
-    segments.append(segment)
+    X = np.array(X)
 
+    print("Feature variance:", np.var(X))  # debug
 
-# Creating dataset for ML as in X
-X = []
-
-for segment in segments:
-    features = extract_features(segment, Fs)
-    X.append(features)
-
-X = np.array(X)
-
-print("Dataset shape:", X.shape) # it checks the shape (1,8) tells us : 1 is the segment with 8 features at once.
+    # -------- SCALING --------
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
